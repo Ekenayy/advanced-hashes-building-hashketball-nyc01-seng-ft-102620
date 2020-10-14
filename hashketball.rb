38d942d36f6b136b245e9fe7452f8bd1987b294d
Ekene N <ekenayy@gmail.com>
2020-10-14 22:59:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,50 @@
 # Write your code here!
+
+def game_hash
+
+hashketball = {
+  :home => 
+    {:team_name => " ",
+     :colors => [],
+     :players =>
+      [
+      {:player_name => "Alan Henderson",
+      :number => "0",
+      :shoe => "16",
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1},
+      {:player_name => "Reggie Evans",
+      :number => "30	",
+      :shoe => "14",
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12	,
+      :blocks => 12,
+      :slam_dunks => 7}
+        ]
+    },
+  :away => 
+   {:team_name => " ",
+     :colors => [] ,
+     :players =>
+      [
+      {:player_name => nil },
+      {:number => nil},
+      {:shoe => nil},
+      {:points => nil},
+      {:rebounds => nil},
+      {:assists => nil},
+      {:steals => nil},
+      {:blocks => nil},
+      {:slam_dunks => nil}
+        ]
+    }
+}
+
+p hashketball
+end 
